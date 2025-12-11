016faba50f7cfa4a0e5b50a040f2c692ae3f2b9f
abhishek29077 <abhishek29072005@gmail.com>
2025-12-11 10:45:55 +0530

    Add files via upload

---
+++ js/breed.js
@@ -0,0 +1,25 @@
+const data = {
+      dog:
+        [
+            "Labrador",
+            "German Shepherd",
+            "Golden Retriever",
+            "Labrador Retriever",
+            "Pug",
+            "Siberian Husky"
+        ],
+        cat:
+        [
+            "Persian",
+            "Maine Coon",
+            "Bengal",
+            "Siamese"
+        ],
+        rabbit:
+        [
+            "Holland Lop",
+            "Angora",
+            "Mini Lop",
+            "Flemish Giant"
+        ]
+    };
